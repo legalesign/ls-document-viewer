09efcb4bccacde892e0467c5ee24939ceb63faff
JodieMac <jodie.macpherson@legalesign.io>
2025-09-23 16:08:38 +0100

    toolbar styling

--- src/components/ls-toolbar/ls-toolbar.tsx
+++ src/components/ls-toolbar/ls-toolbar.tsx
@@ -7,16 +7,14 @@ import { LSApiElement, LSApiTemplate, LsDocumentViewer } from '../../components'
   shadow: true,
 })
 export class LsToolbar {
-
-
   /**
- * The selected items information (as JSON).
- * {LSApiElement[]}
- */
+   * The selected items information (as JSON).
+   * {LSApiElement[]}
+   */
   @Prop({
-    mutable: true
-  }) dataItem: LSApiElement[];
-
+    mutable: true,
+  })
+  dataItem: LSApiElement[];
 
   /**
    * The base template information (as JSON).
@@ -24,38 +22,39 @@ export class LsToolbar {
    */
   @Prop() template: LSApiTemplate;
 
-    /**
+  /**
    * The base template information (as JSON).
    * {LSDocumentViewer}
    */
   @Prop() editor: LsDocumentViewer;
-  
+
   render() {
     return (
       <Host>
-        {this.dataItem && this.dataItem.length > 1 ?
-
-          <div class={"rowbox"}>
-            <select class='ls-participant-select'>
+        {this.dataItem && this.dataItem.length > 1 ? (
+          <div class={'rowbox'}>
+            <select class="ls-participant-select">
               <option value="0">Sender</option>
-              {this.template.roles.map(r => <option value={r.id}>{r.name}</option>)}
-
+              {this.template.roles.map(r => (
+                <option value={r.id}>{r.name}</option>
+              ))}
             </select>
             <ls-field-format dataItem={this?.dataItem} />
             <ls-field-alignment dataItem={this.dataItem} />
             <ls-field-distribute dataItem={this.dataItem} />
             <ls-field-size dataItem={this.dataItem} />
-
           </div>
-          :
-          <div class={"rowbox"}>
-            <select class='ls-participant-select'>
+        ) : (
+          <div class={'rowbox'}>
+            <select class="ls-participant-select">
               <option value="0">Sender</option>
-              {this.template.roles.map(r => <option value={r.id}>{r.name}</option>)}
+              {this.template.roles.map(r => (
+                <option value={r.id}>{r.name}</option>
+              ))}
             </select>
-            <ls-field-format dataItem={this?.dataItem} />
+            {this.dataItem && this.dataItem.length === 1 && <ls-field-format dataItem={this?.dataItem} />}
           </div>
-        }
+        )}
         <slot></slot>
       </Host>
     );
