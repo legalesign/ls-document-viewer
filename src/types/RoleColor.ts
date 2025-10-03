38bee066c2bfdf86c50ac547455ea6c768a7132e
Alex Weinle <alex.weinle@gmail.com>
2025-10-03 14:32:16 +0100

    more colors

--- src/types/RoleColor.ts
+++ src/types/RoleColor.ts
@@ -1,8 +1,13 @@
 export type RoleColor = {
     s10: string
     s20: string
+    s30?: string
+    s40?: string
     s50: string
     s60: string
+    s70?: string
     s80: string
+    s90?: string
+    s100?: string
     description: string
 };
