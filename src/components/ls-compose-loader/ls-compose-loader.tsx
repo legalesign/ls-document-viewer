47fdcb65304f43e37e9d484aae05d2a14264e3b8
JodieMac <jodie.macpherson@legalesign.io>
2026-01-27 11:21:57 +0000

    fix colour classes on loader

--- src/components/ls-compose-loader/ls-compose-loader.tsx
+++ src/components/ls-compose-loader/ls-compose-loader.tsx
@@ -11,12 +11,9 @@ export class LsComposeLoader {
       <Host>
         <div class="compose-loader">
           <div class="compose-loader__header">
-            header?
-            <div class="compose-loader__header-bar">
-              <span class="compose-loader__header-title">Quick Send / Preparation</span>
-              <span class="compose-loader__header-docname"></span>
-            </div>
+            <div class="compose-loader__header-skeleton"></div>
           </div>
+
           <div class="compose-loader__main">
             <div class="compose-loader__sidebar">
               <div class="compose-loader__card">
@@ -25,31 +22,37 @@ export class LsComposeLoader {
                   <div class="compose-loader__skeleton skeleton-line"></div>
                   <div class="compose-loader__skeleton skeleton-short"></div>
                 </div>
-                <div class="compose-loader__section">
-                  <div class="compose-loader__skeleton-box"></div>
-                  <div class="compose-loader__skeleton skeleton-line"></div>
-                  <div class="compose-loader__skeleton skeleton-short"></div>
+                <div class="compose-loader__section compose-loader__section--gray">
+                  <div class="compose-loader__skeleton-box--gray"></div>
+                  <div class="compose-loader__skeleton skeleton-line--gray"></div>
+                  <div class="compose-loader__skeleton skeleton-short--gray"></div>
                 </div>
                 <div class="compose-loader__section compose-loader__section--blue">
-                  <div class="compose-loader__skeleton-box"></div>
-                  <div class="compose-loader__skeleton skeleton-line"></div>
-                  <div class="compose-loader__skeleton skeleton-short"></div>
+                  <div class="compose-loader__skeleton-box--blue"></div>
+                  <div class="compose-loader__skeleton skeleton-line--blue"></div>
+                  <div class="compose-loader__skeleton skeleton-short--blue"></div>
                   <div class="compose-loader__row">
-                    <div class="compose-loader__icon-skeleton"></div>
+                    <div class="compose-loader__icon-skeleton--blue">
+                      <div class="compose-loader__icon--blue"></div>
+                    </div>
                     <div class="compose-loader__row-content">
                       <div class="compose-loader__skeleton skeleton-medium"></div>
                       <div class="compose-loader__skeleton skeleton-icon"></div>
                     </div>
                   </div>
                   <div class="compose-loader__row">
-                    <div class="compose-loader__icon-skeleton"></div>
+                    <div class="compose-loader__icon-skeleton--blue">
+                      <div class="compose-loader__icon--blue"></div>
+                    </div>
                     <div class="compose-loader__row-content">
                       <div class="compose-loader__skeleton skeleton-medium"></div>
                       <div class="compose-loader__skeleton skeleton-icon"></div>
                     </div>
                   </div>
                   <div class="compose-loader__row">
-                    <div class="compose-loader__icon-skeleton"></div>
+                    <div class="compose-loader__icon-skeleton--blue">
+                      <div class="compose-loader__icon--blue"></div>
+                    </div>
                     <div class="compose-loader__row-content">
                       <div class="compose-loader__skeleton skeleton-medium"></div>
                       <div class="compose-loader__skeleton skeleton-icon"></div>
@@ -57,25 +60,31 @@ export class LsComposeLoader {
                   </div>
                 </div>
                 <div class="compose-loader__section compose-loader__section--green">
-                  <div class="compose-loader__skeleton-box"></div>
-                  <div class="compose-loader__skeleton skeleton-line"></div>
-                  <div class="compose-loader__skeleton skeleton-short"></div>
+                  <div class="compose-loader__skeleton-box--green"></div>
+                  <div class="compose-loader__skeleton skeleton-line--green"></div>
+                  <div class="compose-loader__skeleton skeleton-short--green"></div>
                   <div class="compose-loader__row">
-                    <div class="compose-loader__icon-skeleton"></div>
+                    <div class="compose-loader__icon-skeleton--green">
+                      <div class="compose-loader__icon--green"></div>
+                    </div>
                     <div class="compose-loader__row-content">
                       <div class="compose-loader__skeleton skeleton-medium"></div>
                       <div class="compose-loader__skeleton skeleton-icon"></div>
                     </div>
                   </div>
                   <div class="compose-loader__row">
-                    <div class="compose-loader__icon-skeleton"></div>
+                    <div class="compose-loader__icon-skeleton--green">
+                      <div class="compose-loader__icon--green"></div>
+                    </div>
                     <div class="compose-loader__row-content">
                       <div class="compose-loader__skeleton skeleton-medium"></div>
                       <div class="compose-loader__skeleton skeleton-icon"></div>
                     </div>
                   </div>
                   <div class="compose-loader__row">
-                    <div class="compose-loader__icon-skeleton"></div>
+                    <div class="compose-loader__icon-skeleton--green">
+                      <div class="compose-loader__icon--green"></div>
+                    </div>
                     <div class="compose-loader__row-content">
                       <div class="compose-loader__skeleton skeleton-medium"></div>
                       <div class="compose-loader__skeleton skeleton-icon"></div>
@@ -88,7 +97,7 @@ export class LsComposeLoader {
               <div class="compose-loader__preview-box"></div>
             </div>
             <div class="compose-loader__button">
-              <button class="compose-loader__action-btn" disabled>
+              <button class="compose-loader__action-btn">
                 <div class="compose-loader__skeleton skeleton-btn"></div>
                 <div class="compose-loader__skeleton skeleton-btn-icon"></div>
               </button>
